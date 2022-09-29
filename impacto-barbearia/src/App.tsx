1ecbff89f7c8de847a38e7a0e44cdab63dfd8594
Lucas Silva <lucaslima@pecege.com>
2022-09-29 20:26:09 -0300

    ajusta toggle da navbar

--- impacto-barbearia/src/App.tsx
+++ impacto-barbearia/src/App.tsx
@@ -9,20 +9,22 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <nav className='navbar navbar-expand-lg'>
-          <a className="navbar-brand" href="/">
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <Link to="/" className='nav-link'>Agendamentos</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to="/clientes" className='nav-link'>Clientes</Link>
-              </li>
-            </ul>
+          <div className='container-fluid'>
+            <a className="navbar-brand" href="/">
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <Link to="/" className='nav-link'>Agendamentos</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to="/clientes" className='nav-link'>Clientes</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
         <Routes>
